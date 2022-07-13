460b8366f692cf3a009dbd57a4120d59db2368cc
engineer otikojairus1 <otikojairus@yahoo.com>
2022-07-13 18:49:34 -0400

    added tailwind library for react native

--- uba/App.js
+++ uba/App.js
@@ -3,9 +3,12 @@ import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
+
+
 export default function App() {
   return (
     <Provider store={store}>
+
       <HomeScreen />
     </Provider>
   );
--- uba/screens/HomeScreen.js
+++ uba/screens/HomeScreen.js
@@ -1,11 +1,25 @@
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
 import tw from "twrnc";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAP_APIKEY} from "@env"
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
+
       <Text style={tw`text-red-500`}>HomeScreen</Text>
+      <GooglePlacesAutocomplete
+      placeholder='Search'
+      onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+        console.log(data, details);
+      }}
+      query={{
+        key: GOOGLE_MAP_APIKEY,
+        language: 'en',
+      }}
+    />
     </SafeAreaView>
   );
 }
